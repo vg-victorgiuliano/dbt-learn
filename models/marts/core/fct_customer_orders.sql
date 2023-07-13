fcbf6e1d4ac43af01f9f85e2f1ed6a4c2152bb28
victorgiuliano94@gmail.com <victorgiuliano94@gmail.com>
2023-07-13 20:59:11 +0000

    dbt learn

--- models/marts/core/fct_customer_orders.sql
+++ models/marts/core/fct_customer_orders.sql
@@ -1,55 +1,52 @@
-with paid_orders as (
-    select 
-    orders.id as order_id
-    , orders.user_id as customer_id
-    , orders.order_date as order_placed_at
-    , orders.status as order_status
-    , p.total_amount_paid
-    , p.payment_finalized_date
-    , c.first_name as customer_first_name
-    , c.last_name as customer_last_name
-    from {{ source('jaffle_shop', 'order') }} as orders
-    left join (
-        select 
-        orderid as order_id 
-        , max(created) as payment_finalized_date 
-        , sum(amount) / 100.0 as total_amount_paid
-        from {{ source('stripe', 'payment') }}
-        where status <> 'fail'
-        group by 1) p on orders.id = p.order_id
-        left join {{ source('jaffle_shop', 'customer') }} as c 
-        on orders.user_id = c.id 
+-- import CTEs
+
+with customers as(
+    select * from {{ ref('stg_customers') }}
 ),
 
-customer_orders as (
-    select 
-        c.id as customer_id
-        , min(order_date) as first_order_date
-        , max(order_date) as most_recent_order_date
-        , count(orders.id) as number_of_orders
-    from {{ source('jaffle_shop', 'customer') }} as c 
-    left join {{ source('jaffle_shop', 'order') }} as orders
-    on orders.user_id = c.id 
-    group by 1)
+paid_orders as(
+    select * from {{ ref('int_orders') }}
+),
 
-select
-    p.*
-    , row_number() over (order by p.order_id) as transaction_seq
-    , row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq
-    , case when c.first_order_date = p.order_placed_at
-    then 'new'
-    else 'return' end as nvsr
-    , x.clv_bad as customer_lifetime_value
-    , c.first_order_date as fdos
-from paid_orders p
-left join customer_orders as c using (customer_id)
-left outer join (
+-- final CTE
+
+final as(
     select
-        p.order_id
-        , sum(t2.total_amount_paid) as clv_bad
-    from paid_orders p
-    left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
-    group by 1
-    order by p.order_id) as x 
-on x.order_id = p.order_id
-order by order_id
+        {{dbt_utils.generate_surrogate_key(['paid_orders.order_id','paid_orders.customer_id'])}} as sk
+        , paid_orders.order_id
+        , paid_orders.customer_id
+        , paid_orders.order_placed_at
+        , paid_orders.order_status
+        , paid_orders.total_amount_paid
+        , paid_orders.payment_finalized_date
+        , customers.customer_first_name
+        , customers.customer_last_name
+        -- sales transaction sequence
+        , row_number() over (order by paid_orders.order_id) as transaction_seq
+        -- customer sales sequence
+        , row_number() over (partition by paid_orders.customer_id order by paid_orders.order_id) as customer_sales_seq
+        -- new vs return customer
+        , case when (
+            rank() over (
+                partition by paid_orders.customer_id 
+                order by paid_orders.order_placed_at, paid_orders.order_id
+                ) = 1
+        ) then 'new'
+        else 'return' end as nvsr
+
+        -- customer life time value
+        , sum(paid_orders.total_amount_paid) 
+            over (
+                partition by paid_orders.customer_id 
+                order by paid_orders.order_placed_at
+                ) as customer_lifetime_value
+
+            --first day of sale
+        , first_value(paid_orders.order_placed_at) 
+            over (partition by paid_orders.customer_id 
+            order by paid_orders.order_placed_at) as fdos
+    from paid_orders
+    left join customers on paid_orders.customer_id = customers.customer_id
+)
+-- simple select statement
+select * from final
---
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -0,0 +1,14 @@
+with source as(
+    select * from {{ source('jaffle_shop', 'customer') }}
+),
+
+transformed as(
+    select
+        id as customer_id
+        , first_name as customer_first_name
+        , last_name as customer_last_name
+        , first_name || ' ' || last_name as full_name
+    from source
+)
+
+select * from transformed
